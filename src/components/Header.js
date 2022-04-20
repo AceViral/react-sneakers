2d339ab51160cd7eb65d8d8ed0e7f9060d7e6ab1
AceViral <sadsadas-07@mail.ru>
2022-04-20 13:49:24 +0300

    head

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import useCart from "./Hooks/useCart";
-import logo from "../img/logo.png";
-import cart from "../img/cart.svg";
-import like from "../img/like.svg";
-import user from "../img/user.svg";
 function Header(props) {
    const { totalPrice } = useCart();
    return (
